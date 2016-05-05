9dd4c6fa70aca068bac8894792f9f67320bd62b7
joel- <joel-@DESKTOP-B91CPLG>
2016-05-05 05:28:06 -0500

    as

---
+++ ProjectWebClient/Scripts/AppCode.js
@@ -0,0 +1,10 @@
+﻿var mainApp = angular.module('mainApp', ['ngRoute']);
+
+mainApp.run(function ($rootScope) {
+    $rootScope.api = "http://localhost:4247/"
+});
+
+var configFunction = function ($routeProvider, $httpProvider) {
+    $routeProvider.
+        when
+}
---
+++ ProjectWebClient/Scripts/Controllers/ClientsController.js
@@ -0,0 +1,9 @@
+﻿var ClientsController = function ($scope) {
+    $scope.getClients = function ($http) {
+        return $http.get("http://localhost:4247/api/Clients");
+    }
+    $scope.getClients()
+        .success(function (result) {
+            $scope.clients = result;
+        })
+}
